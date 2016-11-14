3f44e6c1e4b6ff6ad305b5a37cd90df8f90fb765
ingaSheremet <inga@tiestegroup.com>
2016-11-14 21:52:40 +0200

    small fix

--- app.js
+++ app.js
@@ -7,8 +7,6 @@
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
-<<<<<<< HEAD
-
         $scope.dishes = "";
         $scope.message = "";
         $scope.checking = function () {
@@ -28,41 +26,7 @@
                 $scope.message = "Too much!";
             }
         };
-
     }
 
 })();
 
-=======
-        $scope.dishes = ' ';
-
-        $scope.message = ' ';
-
-        $scope.checkDishes = function(){
-            var message = $scope.dishes;
-
-            var arrDishes = message.split(',');
-            var number = arrDishes.length;
-
-            var emptyInput = +document.getElementById('lunch-menu').value;
-
-            showAppropriateMessage(number);
-
-            function showAppropriateMessage(number) {
-
-             if(emptyInput ==0){
-                 $scope.message = 'Please enter data first!';
-                 return
-             }
-
-             if (number <= 3) {
-                    $scope.message = 'Enjoy!!!'
-                } else {
-                    $scope.message = 'Too much!'
-                }
-            }
-        };
-    }
-
-})();
->>>>>>> 579aba6925e29ef6613cb59ce2537f88b483405d
